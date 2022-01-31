051a1ffe2ed6ead458a9ec0d5ecb2dd699fdd9d4
unknown <jessedraper2@gmail.com>
2022-01-31 11:40:15 -0700

    change folder structure to clean up work on app.js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import aboutMe from "./components/aboutMe";
+import buttons from "./components/buttons";
+import contactMe from "./components/contactMe";
+import indvProject from "./components/contactMe";
+import navbar from "./components/navbar";
+import projectsList from "./components/projectsList";
+import "./app.css"
+const App = () => {
+  return (
+    <div>
+      <navbar/>
+      <aboutMe/>
+      <projectsList>
+        <indvProject/>
+      </projectsList>
+      <contactMe/>
+    </div>
+  );
+};
