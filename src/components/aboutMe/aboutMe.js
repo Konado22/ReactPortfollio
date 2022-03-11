9bb030b1a2dbb9c1fbd747a4bb71d33e324b7884
unknown <jessedraper2@gmail.com>
2022-03-11 13:11:24 -0700

    wip for today

--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -21,16 +21,17 @@ const AboutMe = () => {
           been an important milestone in my life, as I have strongly desired a
           career change for some time and have always been interested in
           coding/web development. I have been able to gain exposure to many
-          different web languages, frameworks,  and understand their usage! Web development
-          also meshes well with skills I have utilized in previous jobs in
-          hospitality and hospitality management. Some of these skills include
-          problem solving, working with teams, coordinating projects, as well as
-          managing my time effectively. I have really enjoyed the experiences I
-          have gained and have provided some examples of the work I have
-          completed below. The list is continuosly updated with projects I feel
-          more accuratley reflect my skills and interests.Feel free to click
-          them to see the deployed acitivities/respective github links. Please
-          contact me for more information!
+          different web languages, frameworks, and understand their usage! Web
+          development also meshes well with skills I have utilized in previous
+          jobs in hospitality and hospitality management. Some of these skills
+          include problem solving, working with teams, coordinating projects, as
+          well as managing my time effectively. I have really enjoyed the
+          experiences I have gained and have provided some examples of the work
+          I have completed in class or on my freetime. The list is continuosly
+          updated with projects I feel more accuratley reflect my skills and
+          interests.Feel free to navigate to the work page to see the deployed
+          acitivities/respective github links. Please contact me for more
+          information or how I can help your team today!
         </Card.Text>
       </Card>
     </div>
