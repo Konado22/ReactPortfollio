c9ebda0f4b57723b91e2d65991b38a29a7f5b503
unknown <jessedraper2@gmail.com>
2022-03-11 13:05:45 -0700

    wip

--- src/app.js
+++ src/app.js
@@ -1,27 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ContactMePage from "./pages/contactMePage";
-import ExamplesOfWork from "./pages/examplesOfWork";
-import AboutMePage from "./pages/aboutMepage";
-import Homepage from "./pages/homepage";
-import { Navbar } from "react-bootstrap";
 import AppNavbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <AppNavbar />
-      {/* <Router>
-        <Routes>
-          <Route exact path="/ReactPortfollio/" element={<Homepage />} />
-          <Route path="/ReactPortfollio/aboutMe" element={<AboutMePage />} />
-          <Route path="/ReactPortfollio/work" element={<ExamplesOfWork />} />
-          <Route
-            path="/ReactPortfollio/contactMe"
-            element={<ContactMePage />}
-          />
-        </Routes>
-      </Router> */}
     </>
   );
 }
--- src/components/aboutMe/aboutMe.js
+++ src/components/aboutMe/aboutMe.js
@@ -6,31 +6,31 @@ const AboutMe = () => {
         bg="light"
         style={{
           "text-decoration": "none",
-          "paddingLeft": "15px",
-          "paddingRight": "15px",
+          paddingLeft: "15px",
+          paddingRight: "15px",
           // "marginLeft": "15px",
           // "marginRight": "15px"
         }}
       >
         <Card.Title className="bigger">About Me</Card.Title>
         <Card.Text>
-          Hello My Name is Jesse Draper. I am a 26 year old student recently
-          having completed a Full Stack Web Development course through the
-          University Of Denver. Outside of coding I enjoy music, playing in the
-          mountains, and spending time with my 5 year old son. This has been an
-          important milestone in my life, as I have strongly desired a career
-          change for some time and have always been interested in coding/web
-          development. I have been able to gain exposure to many different web
-          languages and understand their usage! Web development also meshes well
-          with skills I have utilized in previous jobs in hospitality and
-          hospitality management. Some of these skills include problem solving,
-          working with teams, coordinating projects, as well as managing my time
-          effectively. I have really enjoyed the experiences I have gained and
-          have provided some examples of the work I have completed below. The
-          list is continuosly updated with projects I feel more accuratley
-          reflect my skills and interests.Feel free to click them to see the
-          deployed acitivities/respective github links. Please contact me for
-          more information!
+          Hello My Name is Jesse Draper. I enjoy music, playing outside in the
+          mountains, and spending time with my 5 year old son. I am a 26 year
+          old student recently having completed a Full Stack Web Development
+          course through the University Of Denver. Outside of coding This has
+          been an important milestone in my life, as I have strongly desired a
+          career change for some time and have always been interested in
+          coding/web development. I have been able to gain exposure to many
+          different web languages, frameworks,  and understand their usage! Web development
+          also meshes well with skills I have utilized in previous jobs in
+          hospitality and hospitality management. Some of these skills include
+          problem solving, working with teams, coordinating projects, as well as
+          managing my time effectively. I have really enjoyed the experiences I
+          have gained and have provided some examples of the work I have
+          completed below. The list is continuosly updated with projects I feel
+          more accuratley reflect my skills and interests.Feel free to click
+          them to see the deployed acitivities/respective github links. Please
+          contact me for more information!
         </Card.Text>
       </Card>
     </div>
